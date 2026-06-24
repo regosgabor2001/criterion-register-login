beb59ea231b6da61222fa7bf11ce3eb922bdc030
regosgabor2001 <regosgabor2001@gmail.com>
2026-06-24 20:15:53 +0200

    fix: add generate csrf token method to request

--- src/http/Request.php
+++ src/http/Request.php
@@ -78,4 +78,16 @@ class Request {
 
         return $this->only(array_keys($rules));
     }
+
+    public static function generateToken(): string {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        
+        if (empty($_SESSION['csrf_token'])) {
+            $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+        }
+        
+        return $_SESSION['csrf_token'];
+    }
 }
