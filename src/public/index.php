457670419a8c38df2afb5c560690b7f02e57467a
regosgabor2001 <regosgabor2001@gmail.com>
2026-06-24 18:44:12 +0200

    fix: add middlewares to routes

--- src/public/index.php
+++ src/public/index.php
@@ -2,7 +2,15 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 use CriterionRegisterLogin\Router\Router;
+use CriterionRegisterLogin\Middleware\AuthMiddleware;
+use CriterionRegisterLogin\Middleware\GuestMiddleware;
 use CriterionRegisterLogin\Controller\RegisterLoginController;
+use CriterionRegisterLogin\Controller\DashboardController;
+use CriterionRegisterLogin\Controller\LogoutController;
 
-Router::get('/register', RegisterLoginController::class . '@registerPage')->name('register.show');
+Router::get('/', RegisterLoginController::class . '@registerLoginpage')->middleware(GuestMiddleware::class);
+Router::post('/register', RegisterLoginController::class . '@register');
+Router::post('/login', RegisterLoginController::class . '@login');
+Router::get('/logout', LogoutController::class . '@logout')->middleware(AuthMiddleware::class);
+Router::get('/dashboard', DashboardController::class . '@index')->middleware(AuthMiddleware::class);
 Router::dispatch();
