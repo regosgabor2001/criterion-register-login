9ddeda4028bad99980ec63973c6e87ec510081a8
regosgabor2001 <regosgabor2001@gmail.com>
2026-06-24 21:06:32 +0200

    fix: add errors and old global to twig

--- src/utility/Twig.php
+++ src/utility/Twig.php
@@ -1,20 +1,31 @@
 <?php
+
 namespace CriterionRegisterLogin\Utility;
 
-class Twig
-{
-    private static $twig;
+class Twig {
+    private $twig;
 
-    public function __construct()
-    {
-        if (self::$twig === null) {
-            $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../view');
-            self::$twig = new \Twig\Environment($loader);
+    public function __construct() {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
         }
+
+        $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../view');
+        $this->twig = new \Twig\Environment($loader, [
+            'cache' => false,
+        ]);
+
+        $errors = $_SESSION['errors'] ?? [];
+        $old = $_SESSION['old'] ?? [];
+
+        $this->twig->addGlobal('errors', $errors);
+        $this->twig->addGlobal('old', $old);
+
+        unset($_SESSION['errors']);
+        unset($_SESSION['old']);
     }
 
-    public function render($template, $data = [])
-    {
-        return self::$twig->render($template, $data);
+    public function render(string $template, array $data = []): string {
+        return $this->twig->render($template, $data);
     }
 }
