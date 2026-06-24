8595445188260350e9a909cade97f7bce181c21d
regosgabor2001 <regosgabor2001@gmail.com>
2026-06-24 21:05:05 +0200

    fix: add rate limiting to routes

--- src/public/index.php
+++ src/public/index.php
@@ -5,13 +5,14 @@ use CriterionRegisterLogin\Router\Router;
 use CriterionRegisterLogin\Middleware\AuthMiddleware;
 use CriterionRegisterLogin\Middleware\GuestMiddleware;
 use CriterionRegisterLogin\Middleware\VerifyCsrfToken;
+use CriterionRegisterLogin\Middleware\RateLimiter;
 use CriterionRegisterLogin\Controller\RegisterLoginController;
 use CriterionRegisterLogin\Controller\DashboardController;
 use CriterionRegisterLogin\Controller\LogoutController;
 
 Router::get('/', RegisterLoginController::class . '@registerLoginpage')->middleware(GuestMiddleware::class);
-Router::post('/register', RegisterLoginController::class . '@register')->middleware(VerifyCsrfToken::class);
-Router::post('/login', RegisterLoginController::class . '@login')->middleware(VerifyCsrfToken::class);
-Router::post('/logout', LogoutController::class . '@logout')->middleware([AuthMiddleware::class, VerifyCsrfToken::class]);
+Router::post('/register', RegisterLoginController::class . '@register')->middleware([VerifyCsrfToken::class, RateLimiter::class]);
+Router::post('/login', RegisterLoginController::class . '@login')->middleware([VerifyCsrfToken::class, RateLimiter::class]);
+Router::post('/logout', LogoutController::class . '@logout')->middleware([AuthMiddleware::class, VerifyCsrfToken::class, RateLimiter::class]);
 Router::get('/dashboard', DashboardController::class . '@index')->middleware(AuthMiddleware::class);
 Router::dispatch();
