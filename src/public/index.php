bbe20c4bfbadbc054a47e8b27472841f14ec0a47
regosgabor2001 <regosgabor2001@gmail.com>
2026-06-24 20:15:03 +0200

    fix: add verify csrf token middleware to routes

--- src/public/index.php
+++ src/public/index.php
@@ -4,13 +4,14 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 use CriterionRegisterLogin\Router\Router;
 use CriterionRegisterLogin\Middleware\AuthMiddleware;
 use CriterionRegisterLogin\Middleware\GuestMiddleware;
+use CriterionRegisterLogin\Middleware\VerifyCsrfToken;
 use CriterionRegisterLogin\Controller\RegisterLoginController;
 use CriterionRegisterLogin\Controller\DashboardController;
 use CriterionRegisterLogin\Controller\LogoutController;
 
 Router::get('/', RegisterLoginController::class . '@registerLoginpage')->middleware(GuestMiddleware::class);
-Router::post('/register', RegisterLoginController::class . '@register');
-Router::post('/login', RegisterLoginController::class . '@login');
-Router::get('/logout', LogoutController::class . '@logout')->middleware(AuthMiddleware::class);
+Router::post('/register', RegisterLoginController::class . '@register')->middleware(VerifyCsrfToken::class);
+Router::post('/login', RegisterLoginController::class . '@login')->middleware(VerifyCsrfToken::class);
+Router::post('/logout', LogoutController::class . '@logout')->middleware([AuthMiddleware::class, VerifyCsrfToken::class]);
 Router::get('/dashboard', DashboardController::class . '@index')->middleware(AuthMiddleware::class);
 Router::dispatch();
